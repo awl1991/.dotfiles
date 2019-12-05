bc673cc8cf489c3e204c3c6396ca575236574d3b
Andrew Labunski <awl1991@gmail.com>
2019-12-05 02:29:45 -0600

    fixed spacing

--- zsh/tools/git-prompt.sh
+++ zsh/tools/git-prompt.sh
@@ -10,17 +10,17 @@ function my_git_formatter() {
     # >=> Foregrounds
     if (( $1 )); then
       # -> Normal status
-      local          end='%f'  # ~> default  fg
+      local          end='%f'   # ~> default  fg
       local      clean='%021F'  # ~> green    fg
       local   modified='%006F'  # ~> yellow   fg
-      local   untracked='%39F'  # ~> blue     fg
+      local   untracked='%24F'  # ~> blue     fg
       local conflicted='%196F'  # ~> red      fg
       local       grey='%243F'  # ~> grey     fg
       local      green='%028F'  # ~> green    fg
       local   gitcolor='%056F'  # ~> git      fg
     else
       # -> Stale/incomplete status
-      local       end='%244F'  # ~> grey     fg
+      local        end='%244F'   # ~> grey    fg
       local      clean='%244F'  # ~> grey     fg
       local   modified='%244F'  # ~> grey     fg
       local  untracked='%019F'  # ~> grey     fg
@@ -33,10 +33,10 @@ function my_git_formatter() {
     local         subs='₀-₁-₂-₃-₄-₅-₆-₇-₈-₉'
     local    divider_i="${grey}⦚${end}"
     local        git_i="${gitcolor}ﯙ"
-    local     branch_i="${clean}$(echo '\uf418')"
+    local     branch_i="${clean}$(echo '')"
     local     staged_i="ﰖ"
     local   unstaged_i=""
-    local  untracked_i="﬒"
+    local  untracked_i=""
     local   conflict_i=""
     # ==> Vcs info
     local   res
@@ -44,7 +44,7 @@ function my_git_formatter() {
 
     # >=> Change branch indicator if master
     if [ $VCS_STATUS_LOCAL_BRANCH = 'master' ]; then
-      branch_i="${clean}$(echo '\uf6a4')"
+      branch_i="${clean}$(echo '')"
     fi
 
     # ~~> >=> Add segments from PK9 <=< <~~ #
@@ -74,7 +74,7 @@ function my_git_formatter() {
     fi
 
     # ->        Behind remote
-    (( VCS_STATUS_COMMITS_BEHIND )) && res+="${space}${clean}%F{009}${VCS_STATUS_COMMITS_BEHIND} ${end}"
+    (( VCS_STATUS_COMMITS_BEHIND )) && res+="${space}${clean}%F{009}${VCS_STATUS_COMMITS_BEHIND} ${end}"
     # -> Clear ahead/!behind
     (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
     # ->     Ahead of remote
@@ -84,13 +84,13 @@ function my_git_formatter() {
     # ->       Unusual state
     [[ -n $VCS_STATUS_ACTION     ]] && res+="${space}${conflicted} ${VCS_STATUS_ACTION} ${end}"
     # ->     Merge conflicts
-    (( VCS_STATUS_NUM_CONFLICTED )) && res+="${space}${conflicted}${conflict_i}$(echo ${VCS_STATUS_NUM_CONFLICTED} | tr $nums $subs) ${end}"
+    (( VCS_STATUS_NUM_CONFLICTED )) && res+="${space}${conflicted}${conflict_i}$(echo ${VCS_STATUS_NUM_CONFLICTED} | tr $nums $subs)${end}"
     # ->      Staged commits
-    (( VCS_STATUS_NUM_STAGED     )) && res+="${space}${green}${staged_i}$(echo ${VCS_STATUS_NUM_STAGED} | tr $nums $subs) ${end}"
+    (( VCS_STATUS_NUM_STAGED     )) && res+="${space}${green}${staged_i}$(echo ${VCS_STATUS_NUM_STAGED} | tr $nums $subs)${end}"
     # ->    Unstaged commits
-    (( VCS_STATUS_NUM_UNSTAGED   )) && res+="${space}${modified}${unstaged_i}$(echo ${VCS_STATUS_NUM_UNSTAGED} | tr $nums $subs) ${end}"
+    (( VCS_STATUS_NUM_UNSTAGED   )) && res+="${space}${modified}${unstaged_i}$(echo ${VCS_STATUS_NUM_UNSTAGED} | tr $nums $subs)${end}"
     # ->     Untracked files
-    (( VCS_STATUS_NUM_UNTRACKED  )) && res+="${space}${untracked}${untracked_i}$(echo ${VCS_STATUS_NUM_UNTRACKED} | tr $nums $subs) ${end}"
+    (( VCS_STATUS_NUM_UNTRACKED  )) && res+="${untracked}${untracked_i}$(echo ${VCS_STATUS_NUM_UNTRACKED} | tr $nums $subs)${end}"
 
     typeset -g my_git_format=$res
   }
