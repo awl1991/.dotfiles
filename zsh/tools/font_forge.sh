27816517def19478a640ad982d3612697c994c03
Andrew Labunski <awl1991@gmail.com>
2019-12-06 20:40:20 -0600

    status message styling

--- zsh/tools/font_forge.sh
+++ zsh/tools/font_forge.sh
@@ -17,22 +17,22 @@ move_icons() {
 	local macfont="/Users/awl/Desktop/AWL/Fonts/Weather Icons Mac.ttf"
 	# check if
 	if [ ! -f $newfont ]
-		then print -P "%F{01}  %F{14}No New Font in Directory %F{01}"
+		then print -P "%F{01}  %F{14}No New Font in Directory  %F{01}"
 	else
 		print -P $(rm "$oldfont")
-		print -P '%F{244} 1. --> %F{14}...Removed old Icon Font %F{244}﯊ %F{10}'
+		print -P '%F{244} 1. --> %F{14}...Removed old Icon Font %F{244}﯊  %F{10}'
 		print -P $(mv "$newfont" "$glyphdir")
-		print -P '%F{244} 2. --> %F{14}...Moved Icon Font to Source %F{244} %F{10}'
+		print -P '%F{244} 2. --> %F{14}...Moved Icon Font to Source %F{244}  %F{10}'
 		print -P $(mv $rename $oldfont)
-		print -P '%F{244} 3. --> %F{14}...Renamed New Font %F{244} %F{10}'
+		print -P '%F{244} 3. --> %F{14}...Renamed New Font %F{244}  %F{10}'
 		print -P $(rm $macfont)
-		print -P '%F{244} 4. --> %F{14}...Removed generated Mac Font %F{244}﯊ %F{10}'
+		print -P '%F{244} 4. --> %F{14}...Removed generated Mac Font %F{244}﯊  %F{10}'
 	fi
 
 	if [ ! -f $htmlfile ]
-		then print -P '\n\n %F{244} -- >=> %F{14}Done %F{034}\n'
+		then print -P '\n\n %F{244} -- >=> %F{14}Done  %F{034}\n'
 	else
 		print -P $(rm $htmlfile)
-		print -P '%F{244} 5. --> %F{14}...Removed generated html file %F{244} %F{10}\n\n %F{244}-- >=> %F{14}Done %F{034}\n'
+		print -P '%F{244} 5. --> %F{14}...Removed generated html file %F{244} %F{10}\n\n %F{244} >=> %F{14}Done  %F{034}\n'
 	fi
 }
