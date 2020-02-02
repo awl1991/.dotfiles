67bb3079d8057e3683baa3a64ccf09f1179d6a5a
Andrew Labunski <awl1991@gmail.com>
2020-02-02 02:10:11 -0600

    echo_path.sh icon change

--- zsh/tools/echo_path.sh
+++ zsh/tools/echo_path.sh
@@ -11,5 +11,5 @@ function chpwd() {
    local nums='0-1-2-3-4-5-6-7-8-9'
    local subs='₀-₁-₂-₃-₄-₅-₆-₇-₈-₉'
 
-   print -P "%F{244}$icon%f%f%F{244}${PWD##*/}%f\n$space%F{236}├╴%f%F{240}%f %F{242}$(echo $dirs | tr $nums $subs)%F{236}\n$space%F{236}└╴%f%F{240}%f %F{242}$(echo $files | tr $nums $subs)%f"
+   print -P "%F{244}$icon%f%f%F{244}${PWD##*/}%f\n$space%F{236}├╴%f%F{240} %f%F{242}$(echo $dirs | tr $nums $subs)%F{236}\n$space%F{236}└╴%f%F{240} %f%F{242}$(echo $files | tr $nums $subs)%f"
 }
